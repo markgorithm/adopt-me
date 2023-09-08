6da947a304090954ccbc1ff776f5cfe418c27834
Marcos Godínez Cid de la Paz <markgorithm@Marcoss-MacBook-Pro.local>
2023-09-07 21:43:17 -0600

    02.03. Pure React Components
    Brian demonstrates breaking up code into React components for more compartmentalized organization and how to insert data from props. React utilizes a one-way data flow from parent to child, simplifying debugging by narrowing down where the introduction of the error occurred.

---
+++ src/App.js
@@ -0,0 +1,34 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ])
+}
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    [React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: 'Cat',
+      name: 'Sofiberto',
+      breed: 'Orange'
+    }),
+    React.createElement(Pet, {
+      animal: 'Cat',
+      name: 'Chikistrikis',
+      breed: 'Mixed'
+    }),
+    React.createElement(Pet, {
+      animal: 'Cat',
+      name: 'Chuy Alberto',
+      breed: 'Orange'
+    }),]
+  )
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
