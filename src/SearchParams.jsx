06c992057358483c6562c5bdb6dd972138c7c445
Marcos Cid de la Paz <im@markgorithm.dev>
2023-09-10 21:27:52 -0600

    03.06. Adding animal breed
    Brian walks through creating a breed selection that will be disabled if no animal type is selected and updates the breed type to an empty string if a new animal type is selected.

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
-const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"]
+import { useState } from "react";
+const animals = ["bird", "cat", "dog", "rabbit", "reptile"];
+const breeds = [];
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("")
-  const [animal, setAnimal] = useState("")
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
   return (
     <div className="search-params">
       <form>
@@ -16,22 +18,43 @@ const SearchParams = () => {
             placeholder="Location"
           />
         </label>
-        <label htmlFor='animal'>
+        <label htmlFor="animal">
           Animal
           <select
             id="animal"
             value={animal}
-            onChange={
-              e => setAnimal(e.target.value)
-            }>
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setAnimal("")
+            }}
+          >
             <option />
-            {ANIMALS.map((animal) => {
+            {animals.map((animal) => (
               <option key={animal}>{animal}</option>
-            })}
-        </select>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            disabled={breeds.length === 0}
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
         </label>
       </form>
-    </div>)
-}
+    </div>
+  );
+};
 
-export default SearchParams
+export default SearchParams;
